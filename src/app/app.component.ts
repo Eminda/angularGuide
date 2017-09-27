6026e43a74970c117063319d913d68d442595909
eminda <eminda.desilva@proavos.com>
2017-09-27 14:22:19 +0530

    Data Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
-
+import {Component} from '@angular/core';
+import {FormsModule} from '@angular/forms'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  heading = 'Hi,There';
+  paragraphClasses = 'font-color font-size';
+  value = 'I am the value';
+  valueWithEvent = 'I am value with event';
+  ngModelValue = 'I am the ngModel Value';
+
+  ngModelValueWithEvent = 'I am the ngModel Value';
 }
