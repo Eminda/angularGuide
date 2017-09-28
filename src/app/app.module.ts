30683b6e1e236d555cc34ca364b426529edf2a55
eminda <eminda.desilva@proavos.com>
2017-09-28 13:59:17 +0530

    Form Completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {DataBindingComponent} from './data-binding/data-binding.component';
 import {AngularDirectivesComponent} from './angular-directives/angular-directives.component';
 import {Switch1Component} from './angular-directives/switch1/switch1.component';
@@ -11,11 +11,15 @@ import {Switch3Component} from './angular-directives/switch3/switch3.component';
 import {RouterModule, Routes} from "@angular/router";
 import {FormComponent} from './form/form.component';
 import {Form1Component} from './form/form1/form1.component';
-import { Form2Component } from './form/form2/form2.component';
-import { Form3Component } from './form/form3/form3.component';
-import { Form4Component } from './form/form4/form4.component';
-import { BirthYearDirective } from './form/form4/birth-year.directive';
-import { Form5Component } from './form/form5/form5.component';
+import {Form2Component} from './form/form2/form2.component';
+import {Form3Component} from './form/form3/form3.component';
+import {Form4Component} from './form/form4/form4.component';
+import {BirthYearDirective} from './form/form4/birth-year.directive';
+import {Form5Component} from './form/form5/form5.component';
+import {Form6Component} from './form/form6/form6.component';
+import { Form7Component } from './form/form7/form7.component';
+import { Form8Component } from './form/form8/form8.component';
+import { Form9Component } from './form/form9/form9.component';
 
 const appRoutes: Routes = [
   {path: 'directive', component: AngularDirectivesComponent},
@@ -26,7 +30,11 @@ const appRoutes: Routes = [
     {path: 'form2', component: Form2Component},
     {path: 'form3', component: Form3Component},
     {path: 'form4', component: Form4Component},
-    {path: 'form5', component: Form5Component}
+    {path: 'form5', component: Form5Component},
+    {path: 'form6', component: Form6Component},
+    {path: 'form7', component: Form7Component},
+    {path: 'form8', component: Form8Component},
+    {path: 'form9', component: Form9Component}
   ]
   }
 ];
@@ -46,14 +54,18 @@ const appRoutes: Routes = [
     Form3Component,
     Form4Component,
     BirthYearDirective,
-    Form5Component
+    Form5Component,
+    Form6Component,
+    Form7Component,
+    Form8Component,
+    Form9Component
   ],
   imports: [
     BrowserModule, FormsModule, RouterModule.forRoot(
       appRoutes
       // ,
       // {enableTracing: true} // <-- debugging purposes only
-    )
+    ), ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
